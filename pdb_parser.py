f0d47f5dd4ab962fcf7d35758a1ba3c540ef9ced
Arjun Srinivasan <nintendo.berserker@gmail.com>
2011-06-19 23:51:30 -0400

    Fixed docstrings in pdb_parser, added preliminary potential support to the tessellation program.

--- pdb_parser.py
+++ pdb_parser.py
@@ -13,29 +13,29 @@ PDBFTP = "http://pdbbeta.rcsb.org/pdb/files/"
 PDBEXT = ".pdb"
 
 def download_pdb_file(pdbid):
-	''' Downloads a pdb file from the Protein Data Bank (PDB)
+	""" Downloads a pdb file from the Protein Data Bank (PDB)
 	
 	Arguments:
 	pdbid -- The PDB id of the pdb file.
 
 	Returns:
 	File-like url object.
-	'''
+	"""
 	return urlopen(PDBFTP + pdbid + PDBEXT)
 
 def open_pdb_file(pdbfile):
-	''' Opens a locally stored PDB file
+	""" Opens a locally stored PDB file
 
 	Arguments:
 	pdbfile -- The filename of the PDB file.
 
 	Returns:
 	File object of the PDB file.
-	'''
+	"""
 	return open(pdbfile, 'rb') 
 
 def parse_pdb_file(pdbfile, handle='all', remark_handle='all'):
-	''' Parses a PDB file line-by-line, sending individual lines to dedicated parsers.
+	""" Parses a PDB file line-by-line, sending individual lines to dedicated parsers.
 
 	Arguments:
 	pdbfile -- File object of PDB file.
@@ -43,7 +43,7 @@ def parse_pdb_file(pdbfile, handle='all', remark_handle='all'):
 
 	Returns:
 	Map of PDB file.
-	'''
+	"""
 	pdbdata = [y for y in map(lambda x: str(x, 'ascii'), pdbfile.readlines())]
 	pdbfile.close()
 	# uses eval to evaluate specific records, using regexes to find said records if the records are in handle.
@@ -51,38 +51,38 @@ def parse_pdb_file(pdbfile, handle='all', remark_handle='all'):
 	
 
 def handle_atom_record(records):
-	''' Handles atom records.
+	""" Handles atom records.
 
 	Arguments:
 	records -- All atom records.
 
 	Returns:
 	A list of dictionaries of related information.
-	'''
+	"""
 	return [{'record' : 'ATOM', 'serial' : int(x[6:11].strip()), 'name' : x[12:16].strip(), 'altloc' : x[16], 'res' : x[17:20], 'chain' : x[21], 'resseq' : int(x[22:26].strip()), 'icode' : x[26], 'coord' : array([float(x[30:38].rstrip()), float(x[38:46].rstrip()), float(x[46:54].rstrip())]), 'occupancy' : float(x[54:60].rstrip()), 'temp' : float(x[60:66].rstrip()), 'element' : x[76:78].lstrip(), 'charge' : x[78:80]} for x in records]
 
 
 def handle_remark_record(records, remark_handle):
-	'''Handles remark records by passing them on to helper functions for individual remarks.
+	""" Handles remark records by passing them on to helper functions for individual remarks.
 
 	Arguments:
 	records -- All remark records.
 
 	Returns:
 	A dictionary containing information for each type of remark.
-	'''
+	"""
 	# essentially identical in idea to parse_pdb_file, except it does it for remark numbers.
 	return {int(z): eval("handle_remark_"+z+"_record("+str(findall(r'^REMARK'+r'\s*'+z+r'.*\n', ''.join(records), MULTILINE))+")") for z in {x[6:10].strip() for x in records} if int(z) in remark_handle}
 
 def handle_remark_465_record(records):
-	''' Handles remark 465, missing residues record.
+	""" Handles remark 465, missing residues record.
 
 	Arguments:
 	records -- Missing residue records.
 
 	Returns:
 	List of missing residues and their numbers (tuple).
-	'''
+	"""
 	return [{'res' : x[15:18], 'chain' : x[19], 'resseq' : int(x[21:26].strip()), 'icode' : x[26]} for x in records[7:]]
 
 
