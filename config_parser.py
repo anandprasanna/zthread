998cb103dcc381b896459f0081d4a8ca8bc48098
Arjun Srinivasan <nintendo.berserker@gmail.com>
2011-07-05 12:56:54 -0400

    Fixed slow residue potential finder.

--- config_parser.py
+++ config_parser.py
@@ -18,24 +18,26 @@ if __name__ == '__main__':
 		pdbfile = download_pdb_file(PDB_id)
 	else:
 		pdbfile = open_pdb_file(PDB_filename)
-	if FASTA_download:
-		ffile = download_fasta_file(FASTA_id, FASTA_database)
-	else:
-		ffile = open_fasta_file(FASTA_filename)
-	seq = [x[1] for x in parse_fasta_file(ffile)]
 	pdbdata = parse_pdb_file(pdbfile, records_parse, records_remarks_parse)
 	atoms = filter_target(pdbdata, name=atom_name, span=span, chain=chain)
-	seq = trim_gaps(seq)
 	verts = tessellate(atoms).vertices
 	sim_pot = simplex_potential([[atoms[x]['res'] for x in y] for y in verts]) 
-	res_pot = residue_potential(atoms, verts, sim_pot)
-	for z in seq[1:]:
-		residues = thread_sequence(z, pdbdata)
-		simd = simplex_potential([[residues[x] for x in y] for y in verts]) 
-		resd = residue_potential(atoms, verts, simd)
-		deltaq = array(resd)-array(res_pot)
-		if output_csv:
-			mkdir(data_dir+task_name)
-			csvfile = open_csv_file(data_dir+task_name+"/output.csv")
-			write_threading_output(csvfile, 
+	res_pot = residue_potential(len(atoms), verts, sim_pot)
+	print(sum(res_pot))
+	if task_thread: 	
+		if FASTA_download:
+			ffile = download_fasta_file(FASTA_id, FASTA_database)
+		else:
+			ffile = open_fasta_file(FASTA_filename)
+		seq = [x[1] for x in parse_fasta_file(ffile)]
+		seq = trim_gaps(seq)
+		for z in seq[1:]:
+			residues = thread_sequence(z, pdbdata)
+			simd = simplex_potential([[residues[x] for x in y] for y in verts]) 
+			resd = residue_potential(atoms, verts, simd)
+			deltaq = array(resd)-array(res_pot)
+			if output_csv:
+				mkdir(data_dir+task_name)
+				csvfile = open_csv_file(data_dir+task_name+"/output.csv")
+#				write_threading_output(csvfile, 
 
