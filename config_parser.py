a107594f4df92b9497a501e3b9d0d3d4414c2eee
Arjun Srinivasan <nintendo.berserker@gmail.com>
2011-07-08 12:22:18 -0400

    Output to CSV works for threading now.

--- config_parser.py
+++ config_parser.py
@@ -8,13 +8,12 @@ from pdb_parser import *
 from ptessellation import *
 from pprofile import *
 from pthreading import *
-from output_csv import *
 
 from Bio import AlignIO
 from Bio.Entrez import efetch
 
 from os import mkdir
-import sys
+import sys,csv
 
 if __name__ == '__main__':
     # Evaluate config file
@@ -31,7 +30,8 @@ if __name__ == '__main__':
         # Filter the atoms by name, then find optimal span, if necessary
         atoms = filter_target(pdbdata['ATOM'], name=atom_name, span=span, chain=chain)
         if optimal_span:
-            atoms = filter_target(atoms, name=atom_name, span=find_optimal_span(atoms), chain=chain)
+            span = find_optimal_span(atoms)
+            atoms = filter_target(atoms, name=atom_name, span=span, chain=chain)
 
         # Tessellate protein
         verts = tessellate(atoms).vertices
@@ -44,21 +44,20 @@ if __name__ == '__main__':
 #	for x,y,z in zip([i['resseq'] for i in atoms],[i['res'] for i in atoms], res_pot):
 #		print(x,y,z)
 
-    print(sum(orig_res_pot))
-    
     if task_thread: 	
         if seq_download:
             seqfile = efetch(db="protein", id=seq_id, rettype=seq_format)
         else:
             seqfile = seq_filename
-        seq = [x.seq for x in AlignIO.read(seqfile,seq_format)]
+        ids,seq = zip(*[(x.id, x.seq) for x in AlignIO.read(seqfile,seq_format)])
         trimmed_nums, trimmed_seq = zip(*[trim_gaps(x,seq[0], number_sequence(x)) for x in seq[1:]])
-        for z,i in zip(trimmed_seq[1:], trimmed_nums[1:]):
+        for z,i,d in zip(trimmed_seq, trimmed_nums, ids):
             res_numbers,residues = thread_sequence(z, seq[0], pdbdata['DBREF']['seqbegin'], i, span)
             mut_sim_pot = simplex_potential([[residues[x] for x in y] for y in verts]) 
             mut_res_pot = residue_potential(len(atoms), verts, mut_sim_pot)
             deltaq = array(mut_res_pot)-array(orig_res_pot)
             if output_csv:
-                csvfile = open_csv_file(data_dir+task_name+"/output.csv")
-                write_threading_output(csvfile, span, [x['res'] for x in atoms], orig_res_pot, res_numbers, residues, mut_res_pot)
+                csvfile = csv.writer(open(data_dir+task_name+"/output_"+d+".csv", 'w'), delimiter='\t')
+                args = (span, [x['res'] for x in atoms], orig_res_pot, res_numbers, residues, mut_res_pot, deltaq)
+                for a in zip(*args): csvfile.writerow(a)
 
