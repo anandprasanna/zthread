0af621e05499eed548fbd83e55183ef0490760d4
unknown <Anand@.(none)>
2011-07-21 11:28:19 -0400

    Added methods to make a sec struc dic from dssp
    adds more functionality

--- pdb_parser.py
+++ pdb_parser.py
@@ -3,14 +3,18 @@
 # Written by Arjun Srinivasan
 
 import sys
-
+import BeautifulSoup
+import itertools
 from urllib import urlopen
 from re import findall, MULTILINE
 from numpy import array
-
+import re
 # url for pdb file downloads
 PDBFTP = "http://pdbbeta.rcsb.org/pdb/files/"
-
+#url for dssp seq
+DSSPFTP = "http://www.pdb.org/pdb/explore/sequenceText.do?structureId="
+#specified chain
+DSSPCHAIN = "&chainId="
 # file extension of pdb files
 PDBEXT = ".pdb"
 
@@ -34,7 +38,7 @@ def open_pdb_file(pdbfile):
     Returns:
     File object of the PDB file.
     """
-    return open(pdbfile, 'r') 
+    return open(pdbfile, 'r')
 
 def parse_pdb_file(pdbfile, handle='all', remark_handle='all'):
     """ Parses a PDB file line-by-line, sending individual lines to dedicated parsers.
@@ -94,8 +98,47 @@ def handle_remark_465_record(records):
     List of missing residues and their numbers (tuple).
     """
     return [{'res' : x[15:18], 'chain' : x[19], 'resseq' : int(x[21:26].strip()), 'icode' : x[26]} for x in records[7:]]
+def make_secondary_struc_dic(pdbid,chainid):
+   # f=open('output.txt','w')
+   # text = ''.join(BeautifulSoup.BeautifulSoup(urlopen(DSSPFTP+pdbid+DSSPCHAIN+chainid)).body(text=True))
+    #listq = []
+    text2 = ''.join(BeautifulSoup.BeautifulSoup(urlopen(DSSPFTP+pdbid+DSSPCHAIN+chainid)).body(text=True)).replace("&nbsp;","C")
+    text2 = re.sub("\d","",text2)# removes all digits
+    text2 = re.sub("\n\s*\n*", "\n", text2).strip()# removed multimple newlines replaces with one
+    listq = (text2.split("\n"))
+    listq.pop()
+    listq.pop(0)
+    listq = [x for x in listq if listq.index(x) %2 != 0]
+    for index, item in enumerate(listq[:]):
+        listq[index] = list(item)
+    for item in listq:
+        y =10
+        for x in xrange(10,len(item),y):
+            y-=1
+            item.pop(x)
+
+
+    return list(itertools.chain.from_iterable(listq))
+
+
+
+
+
+
+
+"""
+    for item in ''.join(BeautifulSoup.BeautifulSoup(urlopen(DSSPFTP+pdbid+DSSPCHAIN+chainid)).body(text=True)).replace("&nbsp;","C").split("\n"):
+        print repr(item)
+        if((item != '' or item != ' ')and  not (item.isdigit())):
+            listq.append(list(item))
+"""
+
+
+
+
+
 
 
 # test code below here
-if __name__ == '__main__':
-    print(parse_pdb_file(open_pdb_file(sys.argv[1]), ['ATOM','REMARK', 'DBREF'], [465]))
+#if __name__ == '__main__':
+ #   print(parse_pdb_file(open_pdb_file(sys.argv[1]), ['ATOM','REMARK', 'DBREF'], [465]))
